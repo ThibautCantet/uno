45852457e91513d8ee0b5c11705f1b45a8be62cf
Thibaut Cantet <thibaut.cantet@gmail.com>
2021-01-08 21:51:23 +0100

    implement find

--- src/main/java/uno/es/infrastructure/StaticDeckRepository.java
+++ src/main/java/uno/es/infrastructure/StaticDeckRepository.java
@@ -2,8 +2,13 @@ package uno.es.infrastructure;
 
 import uno.es.domain.game.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class StaticDeckRepository implements DeckRepository {
+    private final List<DeckEvent> events = new ArrayList<>();
+
     @Override
     public Deck findNewDeck(DeckId deckId) {
         return Deck.createNewDeck(deckId);
@@ -11,11 +16,15 @@ public class StaticDeckRepository implements DeckRepository {
 
     @Override
     public void save(Deck deck) {
-
+        events.addAll(deck.getGeneratedEvents());
     }
 
     @Override
     public Deck find(DeckId deckId) {
         return null;
+    }
+
+    List<DeckEvent> getEvents() {
+        return events;
     }
 }
--- src/test/java/uno/es/infrastructure/StaticDeckRepositoryUTest.java
+++ src/test/java/uno/es/infrastructure/StaticDeckRepositoryUTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uno.es.domain.game.Deck;
 import uno.es.domain.game.DeckId;
+import uno.es.domain.game.DeckShuffledEvent;
 
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 class StaticDeckRepositoryUTest {
 
@@ -27,5 +30,19 @@ class StaticDeckRepositoryUTest {
 
         // then
         assertThat(deck).isEqualTo(expectedDeck);
+    }
+
+    @Test
+    void save_should_save_deck_events() {
+        // given
+        Deck deck = mock(Deck.class);
+        final DeckShuffledEvent deckShuffledEvent = mock(DeckShuffledEvent.class);
+        when(deck.getGeneratedEvents()).thenReturn(singletonList(deckShuffledEvent));
+
+        // when
+        staticDeckRepository.save(deck);
+
+        // then
+        assertThat(staticDeckRepository.getEvents()).usingFieldByFieldElementComparator().containsExactly(deckShuffledEvent);
     }
 }
